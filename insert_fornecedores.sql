9059c7f4401a7928280009a9074712b2817cd02a
decohenriqueandre-arch <decohenriqueandre@gmail.com>
2026-04-06 19:10:57 -0300

    Trabalho Aula 08

---
+++ insert_fornecedores.sql
@@ -0,0 +1,13 @@
+INSERT INTO FORNECEDORES 
+(NOME_FORNECEDOR)
+VALUES
+('Distribuidora Cevada Pura'), 
+('Alambique Velho Oeste'), 
+('Granja Renascer'), 
+('Frigorífico Boi Gordo'), 
+('Salgadaria Popular'), 
+('Hortifruti do Campo'), 
+('Snacks do Cerrado'), 
+('Laticínios da Serra'), 
+('Pescados da Costa'),
+('Tabacaria Ouro Negro'); 
---
+++ insert_funcionarios.sql
@@ -0,0 +1,6 @@
+INSERT INTO FUNCIONARIOS 
+(NOME_FUNCIONARIO, SALARIO, ANO_ENTRADA, CARGO) 
+VALUES 
+('Adônis Grega', 1834.45, 2025, 'Atendente'),
+('Apolo God', 1900.03, 2024, 'Atendente'),
+('Ártemis Sister', 2300.00, 2022, 'Gerente'); 
